27a9fd2adf7d41c2e345aca3022d2ff714d31c61
AiDuduAaaiii <169045870+AiDuduAaaiii@users.noreply.github.com>
2024-10-21 11:32:43 -0300

    .

--- main.js
+++ main.js
@@ -6,3 +6,31 @@ let numeroAleatorio = Math.floor(Math.random() * 100) + 1;
     const campoPalpite = document.querySelector('.campoPalpite');
     let contagemPalpites = 1;
     let botaoReiniciar;
+
+    function verificarPalpite() {
+        const palpiteUsuario = Number(campoPalpite.value);
+        if (contagemPalpites === 1) {
+        palpites.textContent = "Palpites anteriores: ";
+        }
+
+        palpites.textContent += palpiteUsuario + " ";
+
+        if (palpiteUsuario === numeroAleatorio) {
+            ultimoResultado.textContent = "Parabéns! Voce acertou!";
+            ultimoResultado.style.backgroundColor = "green";
+            baixo0uAlto.textContent = "";
+            finalizarJogo();
+        } else if (contagemPalpites === 10) {
+            ultimoResultado.textContent = "FIM DE JOGO! ! !";
+            baixo0uAlto.textContent = "";
+            finalizarJogo();
+        } else {
+            ultimoResultado.textContent = "Errado";
+            ultimoResultado.style.backgroundColor = "red";
+            if (palpiteUsuario < numeroAleatorio) {
+            baixoOuAlto.textContent = "O último palpite foi muito baixo";
+        } else if (palpiteUsuario > numeroAleatorio) {
+          baixoOuAlto.textContent = "O último palpite foi muito alto";
+        }
+    }
+}
